a6cdd68a78d5f4fe15ced3b278fdf1b469c481a6
Andris Reinman <andris@kreata.ee>
2013-05-22 12:01:51 +0300

    removes shortinstall

--- quickinstall.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-wget https://github.com/andris9/artikliotsing/archive/master.tar.gz
-tar -xzwf artikliotsing-master.tar.gz
-mv artikliotsing-master artikliotsing
-rm -rf artikliotsing-master.tar.gz
-cd artikliotsing
-./install.sh
