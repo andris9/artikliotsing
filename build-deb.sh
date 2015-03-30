788758018b0200de629662618774771c4fb8077d
Andris Reinman <andris@kreata.ee>
2015-03-30 18:18:10 +0300

    update

--- build-deb.sh
+++ build-deb.sh
@@ -5,12 +5,12 @@ VERSION=`cd opt/${NAME} && node -e 'console.log(require("./package").version)'`
 
 DIR=`pwd`
 cd opt/$NAME
-npm install
+npm install --production --no-bin-links
 cd $DIR
 
 mkdir -p dist
 rm -rf dist/${NAME}-${VERSION}_*.deb
-fpm -s dir -t deb -n ${NAME} -v $VERSION -C . \
+fpm -s dir -a all -t deb -n ${NAME} --vendor "Andris Reinman" -m "andris@kreata.ee" -v $VERSION -C . \
   -p dist/${NAME}-VERSION_ARCH.deb \
   -d "nodejs" \
   -d "elasticsearch" \
